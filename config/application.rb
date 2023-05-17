3041ae61c2d73d0c54bd5e4e28be4a09f7174a26
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-17 11:48:12 -0500

    :wrench: now generators use slim

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Recipes
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.template_engine = :slim
   end
 end
