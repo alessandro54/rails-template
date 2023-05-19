92e3198ae860da8d4f521a5999522ae0fcc63058
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-18 20:12:10 -0500

    :bug: fixed tailwind wrong syntax for slim

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   content: [
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/views/**/*.{html,html.erb,erb, slim, html.slim}',
+    './app/views/**/*.{html,erb,slim,html.slim}',
     './app/javascript/**/*.ts',
   ],
   theme: {
